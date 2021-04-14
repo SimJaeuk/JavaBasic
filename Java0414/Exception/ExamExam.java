0d005d9e061125cf249f571c2edccb4966c5d3b4
심재욱 <78770230+SimJaeuk@users.noreply.github.com>
2021-04-14 15:47:44 +0900

    Add files via upload

---
+++ Java0414/Exception/ExamExam.java
@@ -0,0 +1,9 @@
+public class ExamExam {
+    public static void main(String[] args) {
+        ExceptionExam0 ex = new ExceptionExam0();      
+        int num = ex.get50thItem(new int[30]);
+        System.out.println("array 배열의 50번째 요소의 값 : "+ num);
+    }
+}
+
+
---
+++ Java0414/MyCalTest.java
@@ -0,0 +1,11 @@
+public class MyCalTest {
+    
+    public static void main(String[] args) {
+        Calculator cal = new MyCal();
+        cal.plus(3, 4);
+        int i = cal.exec(5, 6);
+        System.out.println(i);
+        
+        System.out.println(Calculator.exec2(4,3));
+    }
+}
