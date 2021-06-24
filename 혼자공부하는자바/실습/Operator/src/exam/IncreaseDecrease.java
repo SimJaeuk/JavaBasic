5ffb22edc1dfa541a1851ffb3a7328ac05565aef
심재욱 <jaehook293@gmail.com>
2021-06-24 17:00:13 +0900

    연산자

---
+++ 혼자공부하는자바/실습/Operator/src/exam/IncreaseDecrease.java
@@ -0,0 +1,19 @@
+package exam;
+
+public class IncreaseDecrease {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		
+		int result1 = ++x + 10;
+		int result2 = y++ + 10;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		// ++연산자의 위치에 따른 결과값의 차이
+
+	}
+
+}
---
+++ 혼자공부하는자바/실습/Operator/src/quiz/Quiz2.java
@@ -0,0 +1,12 @@
+package quiz;
+
+public class Quiz2 {
+
+	public static void main(String[] args) {
+		boolean stop = true;
+		while(stop = true) {
+			break;
+		}
+	}
+
+}
