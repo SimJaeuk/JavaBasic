122ef3b0dd8c6bf86c3f96bd8a702ebad3abadd4
심재욱 <jaehook293@gmail.com>
2021-06-26 23:51:55 +0900

    StringEqualsTest

---
+++ 혼자공부하는자바/실습/ReferenceType/src/String/StringEqualsExam.java
@@ -0,0 +1,32 @@
+package String;
+
+public class StringEqualsExam {
+
+	public static void main(String[] args) {
+		String strVar1 = "심재욱";
+		String strVar2 = "심재욱";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("참조가 같음");
+		}else {
+			System.out.println("참조가 다름");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("문자열이 같음");
+		}
+		
+		String strVar3 = new String("심재욱");
+		String strVar4 = new String("심재욱");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("참조가 같음");
+		}else {
+			System.out.println("참조가 다름");
+		}
+		
+		if(strVar3.equals(strVar4)) {
+			System.out.println("문자열이 같음");
+		}
+	}
+}
