820f9b44230d353d3bac488ecca198d112f28c9b
심재욱 <jaehook293@gmail.com>
2021-06-27 06:33:39 +0900

    Array

---
+++ 혼자공부하는자바/실습/ReferenceType/src/String/NullExam.java
@@ -0,0 +1,15 @@
+package String;
+
+public class NullExam {
+
+	public static void main(String[] args) {
+		String hobby = null;
+		
+		String name = "심자바";
+		name = null;
+		
+		// 객체의 모든 참조를 끊어주면 가비지 컬렉터가 자동으로 메모리를 수거함
+
+	}
+
+}
