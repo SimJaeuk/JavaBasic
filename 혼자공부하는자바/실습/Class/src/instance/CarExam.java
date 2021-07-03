529dafea3dbcfcbe244cde3fc4376a756772db41
SimJaeuk <jaehook293@gmail.com>
2021-07-03 23:07:06 +0900

    인스턴스멤버, this

---
+++ 혼자공부하는자바/실습/Class/src/instance/CarExam.java
@@ -0,0 +1,14 @@
+package instance;
+
+public class CarExam {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("Æ÷¸£½¦");
+		Car yourCar = new Car("º¥Ã÷");
+		
+		myCar.run();
+		yourCar.run();
+
+	}
+
+}
