ef645533570169b01d0d262bcd3a595b110815e7
SimJaeuk <jaehook293@gmail.com>
2021-07-08 08:40:27 +0900

    인터페이스

---
+++ 혼자공부하는자바/실습/Interface/src/interface1/RemoteControlExam.java
@@ -0,0 +1,12 @@
+package interface1;
+
+public class RemoteControlExam {
+
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+
+	}
+
+}
---
+++ 혼자공부하는자바/실습/Interface/src/interface1/SmartTelevisionExam.java
@@ -0,0 +1,13 @@
+package interface1;
+
+public class SmartTelevisionExam {
+
+	public static void main(String[] args) {
+		SmartTelevision tv = new SmartTelevision();
+		
+		RemoteControl rc = tv;
+		Searchable searchable = tv;
+
+	}
+
+}
