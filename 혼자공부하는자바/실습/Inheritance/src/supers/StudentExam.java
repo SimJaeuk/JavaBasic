d03fb74e2aef48dce639b0213c649f0cdeb1b7fe
심재욱 <jaehook293@gmail.com>
2021-07-07 19:37:41 +0900

    super

---
+++ 혼자공부하는자바/실습/Inheritance/src/supers/StudentExam.java
@@ -0,0 +1,14 @@
+package supers;
+
+public class StudentExam {
+
+	public static void main(String[] args) {
+		Student student = new Student("½ÉÀÚ¹Ù", "123456-1234567", 1);
+		System.out.println("name : " + student.name);
+		System.out.println("ssn : " + student.ssn);
+		System.out.println("studentNo : " + student.studentNo);
+		
+
+	}
+
+}
