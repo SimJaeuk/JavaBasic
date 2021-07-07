9fd0349fcaa05909a5365de308e979a36f42c470
심재욱 <jaehook293@gmail.com>
2021-07-07 20:46:34 +0900

    abstract

---
+++ 혼자공부하는자바/실습/Inheritance/src/abstractC/PhoneExam.java
@@ -0,0 +1,12 @@
+package abstractC;
+
+public class PhoneExam {
+
+	public static void main(String[] args) {
+		SmartPhone smartphone = new SmartPhone("½ÉÀÚ¹Ù");
+		
+		smartphone.turnOn();
+		smartphone.internetSearch();
+		smartphone.turnOff();
+	}
+}
---
+++ 혼자공부하는자바/실습/Inheritance/src/abstractC/overriding/Dog.java
@@ -0,0 +1,13 @@
+package abstractC.overriding;
+
+public class Dog extends Animal{
+	public Dog () {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("멍멍");
+	}
+	//추상 메소드 재정의
+}
