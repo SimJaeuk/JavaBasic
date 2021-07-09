5d33bd9a2d5f846b57b1d117c69356df14069008
SimJaeuk <jaehook293@gmail.com>
2021-07-10 02:30:01 +0900

    object

---
+++ 혼자공부하는자바/실습/Java.lang/src/object/toString/ToStirngExam.java
@@ -0,0 +1,15 @@
+package object.toString;
+
+import java.util.Date;
+
+public class ToStirngExam {
+
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+
+	}
+
+}
