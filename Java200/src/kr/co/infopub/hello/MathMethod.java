4200fb2d7541125a6c312e9c0c1ccdcdadf26d53
심재욱 <jaehook293@gmail.com>
2021-08-01 14:21:42 +0900

    Java 200

---
+++ Java200/src/kr/co/infopub/hello/MathMethod.java
@@ -0,0 +1,38 @@
+package kr.co.infopub.hello;
+
+import static java.lang.Math.sqrt;
+
+public class MathMethod {
+    public static void main(String[] args) {
+        // sqrt -> 제곱근
+        int a = (int) Math.sqrt(9);
+        System.out.println(a);
+
+        // random -> 1과 0사이의 실수(0 포함)
+        double b = Math.random();
+        System.out.println(b);
+
+        // abs -> 절대값 구하기
+        int c = Math.abs(-50);
+        System.out.println(c);
+
+        // ceil -> 올림
+        // floor -> 내림
+        double d = Math.ceil(34.3);
+        double e = Math.floor(34.7);
+        System.out.println(d);
+        System.out.println(e);
+
+        // pow -> 제곱
+        int f = (int) Math.pow(2, 3);
+        System.out.println(f);
+
+        // max -> 최대값
+        // min -> 최소값
+        int g = (int) Math.max(7, 9);
+        System.out.println(g);
+        int h = (int) Math.min(7, 9);
+        System.out.println(h);
+
+    }
+}
