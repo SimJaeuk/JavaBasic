ecea14fe83bb7bd4104fe20ed5140cd380466942
SimJaeuk <jaehook293@gmail.com>
2022-02-01 15:10:58 +0900

    Object

---
+++ meta/test/src/ch06/ObjectEx01.java
@@ -0,0 +1,25 @@
+package ch06;
+
+class Dog {
+	String name = "≈‰≈‰";	
+}
+
+class Cat {
+	String name = "æﬂøÀ¿Ã";
+}
+
+
+public class ObjectEx01 {
+
+	public static void main(String[] args) {
+		Object o1 = new Dog();
+		Object o2 = new Cat();
+		
+		Dog d1 = (Dog)o1;
+		Cat c1 = (Cat)o2;
+		
+		System.out.println(d1.name);
+		System.out.println(c1.name);
+	}
+
+}
